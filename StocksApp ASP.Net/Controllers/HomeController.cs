31f7087fa5b932d10d54b13cb0057eb3149e59cf
nic00la <nic00la.kaleta@gmail.com>
2024-06-24 15:42:15 +0200

    Options Pattern

--- StocksApp ASP.Net/Controllers/HomeController.cs
+++ StocksApp ASP.Net/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using StocksApp_ASP.Net.Services;
 
 namespace StocksApp_ASP.Net.Controllers;
@@ -6,17 +7,22 @@ namespace StocksApp_ASP.Net.Controllers;
 public class HomeController : Controller
 {
     private readonly FinnhubService _finnhubService;
+    private readonly IOptions<TradingOptions> _tradingOptions;
 
-    public HomeController(FinnhubService finnhubService)
+    public HomeController(FinnhubService finnhubService,
+                          IOptions<TradingOptions> tradingOptions
+    )
     {
         _finnhubService = finnhubService;
+        _tradingOptions = tradingOptions;
     }
 
     [Route("/")]
     public async Task<IActionResult> Index()
     {
         Dictionary<string, object>? responseDictionary =
-            await _finnhubService.GetStockPriceQuote("MSFT");
+            await _finnhubService.GetStockPriceQuote(_tradingOptions.Value
+                .DeafultStockSymbol);
 
         return View();
     }
---
+++ StocksApp ASP.Net/TradingOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace StocksApp_ASP.Net;
+
+public class TradingOptions
+{
+    public string? DeafultStockSymbol { get; set; }
+}
