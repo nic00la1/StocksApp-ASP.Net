ea7794b1349d96321d1c208d089e2cb27616d65d
nic00la <nic00la.kaleta@gmail.com>
2024-06-24 12:01:05 +0200

    Creating HttpRequestMessage

--- StocksApp ASP.Net/Services/MyService.cs
+++ StocksApp ASP.Net/Services/MyService.cs
@@ -9,10 +9,18 @@ public class MyService
         _httpClientFactory = httpClientFactory;
     }
 
-    public void method()
+    public async Task method()
     {
         using (HttpClient httpClient = _httpClientFactory.CreateClient())
         {
+            HttpRequestMessage httpRequestMessage = new()
+            {
+                RequestUri = new Uri("url"),
+                Method = HttpMethod.Get
+            };
+
+            HttpResponseMessage httpResponseMessage = await
+                httpClient.SendAsync(httpRequestMessage);
         }
     }
 }
