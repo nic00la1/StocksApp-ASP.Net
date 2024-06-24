64830c07ba5c935fc422e1b92c8001c1872c9a77
nic00la <nic00la.kaleta@gmail.com>
2024-06-24 11:52:09 +0200

    Creating Basic App ✅✅

---
+++ StocksApp ASP.Net/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp_ASP.Net.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
